db97987a15d74fce47581aa2d859c596167d7749
mericEngin <meric.engin@marsathletic.com>
2024-11-25 15:19:17 +0300

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';  
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'todo';
+  taskInput: string = '';
+  tasks: { name: string, completed: boolean }[] = [];
+
+  addTask() {
+    if (this.taskInput.trim()) {
+      this.tasks.push({
+        name: this.taskInput,
+        completed: false
+      });
+      this.taskInput = '';
+    }
+  }
+
+  removeTask(index: number) {
+    this.tasks.splice(index, 1)
+  }
+
+  toggleComplete(index: number) {
+    this.tasks[index].completed = !this.tasks[index].completed;
+  }
+
+  clear() {
+    this.tasks = [];
+  }
 }
